648df7dae00b02595167904f697a25d3f72eb4f2
Tom Byers <tombye@Tom-Byerss-MacBook-Pro.local>
2011-10-02 20:55:31 +0100

    Adding initial files. Code used in blog post and variant examples of the pattern

---
+++ js/pattern_variant_1.js
@@ -0,0 +1,71 @@
+/*global $, Document */
+/*
+ * Extra notes on all
+ *
+ * 1. 	If you want to see the objects you are creating for each element matched
+ *	you can make them public by storing them in an Array on the pattern object
+ */
+var pattern = (function () {
+	var Constr,
+	    activeClass = 'active';
+
+	Constr = function (elm) {
+		var $elm = $(elm),
+		    $tabs = $elm.find('.accordionTab'),
+		    tabIdx = $tabs.length,
+		    $contentAreas = $elm.find('.accordionContent'),
+		    activeIdx = $tabs.index($tabs.filter('.'+ activeClass)),
+		    that = this,
+		    onClick;
+		
+		onClick = function (idx) {
+			return function (eventObj) {
+				if(activeIdx !== null) {
+					that.close();
+				}
+				if (idx === activeIdx) {
+					activeIdx = null;
+				} else {
+					activeIdx = idx;
+					that.open();
+				}
+			};
+		};
+		
+		this.open = function () {
+			$tabs.eq(activeIdx).addClass(activeClass);
+			$contentAreas.eq(activeIdx).slideDown();
+		};
+		this.close = function () {
+			$tabs.eq(activeIdx).removeClass(activeClass);
+			$contentAreas.eq(activeIdx).slideUp();
+		};
+
+		$contentAreas.hide();
+		$contentAreas.eq(activeIdx).show();
+
+		while (tabIdx--) {
+			$tabs.eq(tabIdx).bind('click', onClick(tabIdx));
+		}
+	};
+
+	return {
+		// 1. A public Array to store the created objects in
+		objs : [],
+		init : function (context) {
+			// Store a reference to the pattern object
+			var that = this;
+
+			if (context === 'undefined') {
+				context = document.body;
+			}
+
+			$('.accordion', context).each(function () {
+				// In this scope, this means the element matched
+				// so we use our that variable to access the patterns object
+				that.objs.push(new Constr(this));
+			});
+		}
+	};
+}());
+pattern.init();
